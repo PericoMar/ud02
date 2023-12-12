49ca927d988f39899f2337890a8dc4ddde4f6ed8
PericoMar <pedromartinez.22@campuscamara.es>
2023-12-12 20:58:04 +0100

    [New] Exercise 4 PHP

---
+++ BOLETIN TEMA 2/ejercicio4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <label for="number">Number 1: </label>
+        <input type="text" name="number1" />
+        <label for="number">Number 2: </label>
+        <input type="text" name="number2" />
+        <select name="operacion" id="operacion">
+            <option value="Sumar">Sumar</option>
+            <option value="Restar">Restar</option>
+            <option value="Multiplicar">Multiplicar</option>
+            <option value="dividir">Dividir</option>
+        </select>
+        <button action="submit">Enviar</button>
+    </form>
+</body>
+
+</html>
