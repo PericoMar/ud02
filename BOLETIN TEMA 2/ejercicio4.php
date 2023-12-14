73119821f90e629dc9103196efd53b46a3199395
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2023-12-14 12:59:46 +0100

    [Update] Exercise 4 PHP Finished

--- BOLETIN TEMA 2/ejercicio4.php
+++ BOLETIN TEMA 2/ejercicio4.php
@@ -14,18 +14,35 @@
         <label for="number">Number 2: </label>
         <input type="text" name="number2" />
         <select name="operacion" id="operacion">
-            <option value="Sumar">Sumar</option>
-            <option value="Restar">Restar</option>
-            <option value="Multiplicar">Multiplicar</option>
-            <option value="dividir">Dividir</option>
+            <option value="suma">Sumar</option>
+            <option value="resta">Restar</option>
+            <option value="multiplicacion">Multiplicar</option>
+            <option value="division">Dividir</option>
         </select>
         <button action="submit">Enviar</button>
     </form>
     <?php
+    $operaciones = [
+        'suma' => function($a, $b) { return $a + $b; },
+        'resta' => function($a, $b) { return $a - $b; },
+        'multiplicacion' => function($a, $b) { return $a * $b;},
+        'division' => function($a, $b) {
+            if($b == 0){
+                throw new Exception('No se puede dividir por cero.');
+            }
+            return $a/$b;
+        }
+    ];
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $number1 = empty($_POST['number1']) ? $_POST['number1'] : '';
-            $number2 = empty($_POST['number2']) ? $_POST['number2'] : '';
-            
+            $number1 = isset($_POST['number1']) ? $_POST['number1'] : '';
+            $number2 = isset($_POST['number2']) ? $_POST['number2'] : '';
+            $operacion = $operaciones[$_POST['operacion']];
+            try{
+                $resultado = $operacion($number1,$number2);
+                echo "El resultado a la " , $_POST['operacion'], " de ",$number1," y ",$number2, " es: ",$resultado;
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
         }
 
     ?>
